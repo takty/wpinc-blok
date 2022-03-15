040724b33aa969a705fdb901556005fae51ada4d
Takuto Yanagida <yanagida@stxst.com>
2022-03-15 09:05:38 +0900

    Add js task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@
  * Gulp file
  *
  * @author Takuto Yanagida
- * @version 2022-03-01
+ * @version 2022-03-14
  */
 
 /* eslint-disable no-undef */
 'use strict';
 
+const SRC_JS   = ['src/assets/**/*.js', '!src/assets/**/*.min.js'];
 const SRC_PHP  = ['src/**/*.php'];
 const SRC_PO   = ['src/languages/**/*.po', '!src/languages/**/wpinc-*.po'];
 const SRC_JSON = ['src/languages/**/*.json'];
@@ -15,6 +16,21 @@ const DIST     = './dist';
 
 const gulp = require('gulp');
 const $    = require('gulp-load-plugins')({ pattern: ['gulp-*', '!gulp-sass'] });
+
+
+// -----------------------------------------------------------------------------
+
+
+const js = () => {
+	if (SRC_JS.length === 0) return done();
+	return gulp.src(SRC_JS, { base: 'src' })
+		.pipe($.plumber())
+		.pipe($.babel())
+		.pipe($.terser())
+		.pipe($.changed(DIST, { hasChanged: $.changed.compareContents, extension: '.min.js' }))
+		.pipe($.rename({ extname: '.min.js' }))
+		.pipe(gulp.dest(DIST));
+};
 
 
 // -----------------------------------------------------------------------------
@@ -56,11 +72,12 @@ const locale = gulp.parallel(po, json);
 
 
 const watch = () => {
+	gulp.watch(SRC_JS, js);
 	gulp.watch(SRC_PHP, php);
 	gulp.watch(SRC_PO, po);
 	gulp.watch(SRC_JSON, json);
 };
 
-exports.build = gulp.parallel(php, locale);
+exports.build = gulp.parallel(js, php, locale);
 
 exports.default = gulp.series(exports.build, watch);
