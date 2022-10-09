562881a497d674d670624bf9034147c0604abb54
Takuto Yanagida <yanagida@stxst.com>
2022-10-09 23:49:46 +0900

    Improve icons

--- src/blocks/card/index.js
+++ src/blocks/card/index.js
@@ -2,7 +2,7 @@
  * Card block
  *
  * @author Takuto Yanagida
- * @version 2022-03-15
+ * @version 2022-10-08
  */
 
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
@@ -11,7 +11,7 @@ import { registerBlockType } from '@wordpress/blocks';
 import './style.scss';
 import './editor.scss';
 
-const icon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><rect width="14" height="14" x="8" y="8" rx="3.5" /><path d="M18.5 26h-7A3.5 3.5 0 0 0 8 29.5v7a3.5 3.5 0 0 0 3.5 3.5h7a3.5 3.5 0 0 0 3.5-3.5v-7a3.5 3.5 0 0 0-3.5-3.5Zm.5 10.5a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5ZM36.5 8h-7a3.5 3.5 0 0 0-3.5 3.5v7a3.5 3.5 0 0 0 3.5 3.5h7a3.5 3.5 0 0 0 3.5-3.5v-7A3.5 3.5 0 0 0 36.5 8Zm.5 10.5a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5Z" /></svg>;
+const icon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><rect width="14" height="14" x="8" y="8" rx="3.5" ry="3.5"/><path d="M18.5 40h-7A3.5 3.5 0 0 1 8 36.5v-7a3.5 3.5 0 0 1 3.5-3.5h7a3.5 3.5 0 0 1 3.5 3.5v7a3.5 3.5 0 0 1-3.5 3.5Zm-7-11a.5.5 0 0 0-.5.5v7c0 .28.22.5.5.5h7a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-7ZM36.5 22h-7a3.5 3.5 0 0 1-3.5-3.5v-7A3.5 3.5 0 0 1 29.5 8h7a3.5 3.5 0 0 1 3.5 3.5v7a3.5 3.5 0 0 1-3.5 3.5Zm-7-11a.5.5 0 0 0-.5.5v7c0 .28.22.5.5.5h7a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-7Z"/></svg>;
 
 function edit() {
 	return (
