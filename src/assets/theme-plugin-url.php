313dd02d57c0d2c2072c981d85e323a14b3169ec
Takuto Yanagida <yanagida@stxst.com>
2022-10-12 15:26:54 +0900

    Minor improvement

--- src/assets/theme-plugin-url.php
+++ src/assets/theme-plugin-url.php
@@ -4,39 +4,41 @@
  *
  * @package Wpinc
  * @author Takuto Yanagida
- * @version 2022-09-10
+ * @version 2022-10-11
  */
 
 namespace wpinc;
 
 require_once __DIR__ . '/asset-url.php';
 
-/**
- * Initializes theme plugin URL.
- */
-function initialize_theme_plugin_url(): void {
-	static $do_once = false;
-	if ( $do_once ) {
-		return;
+if ( ! function_exists( '\wpinc\initialize_theme_plugin_url' ) ) {
+	/**
+	 * Initializes theme plugin URL.
+	 */
+	function initialize_theme_plugin_url(): void {
+		static $do_once = false;
+		if ( $do_once ) {
+			return;
+		}
+		$do_once = true;
+		add_filter( 'plugins_url', '\wpinc\_cb_plugins_url_for_theme', 10, 3 );
 	}
-	$do_once = true;
-	add_filter( 'plugins_url', '\wpinc\_cb_plugins_url_for_theme', 10, 3 );
-}
 
-/**
- * Callback function for 'plugins_url' hook.
- *
- * @access private
- *
- * @param string $url    The complete URL to the plugins directory including scheme and path.
- * @param string $path   Path relative to the URL to the plugins directory. Blank string if no path is specified.
- * @param string $plugin The plugin file path to be relative to. Blank string if no plugin is specified.
- * @return string Filtered URL.
- */
-function _cb_plugins_url_for_theme( string $url, string $path, string $plugin ): string {
-	$theme_dir = wp_normalize_path( defined( 'THEME_PATH' ) ? THEME_PATH : get_stylesheet_directory() );
-	if ( false !== strpos( $plugin, $theme_dir ) ) {
-		return \wpinc\abs_url( \wpinc\get_file_uri( dirname( $plugin ) ), $path );
+	/**
+	 * Callback function for 'plugins_url' hook.
+	 *
+	 * @access private
+	 *
+	 * @param string $url    The complete URL to the plugins directory including scheme and path.
+	 * @param string $path   Path relative to the URL to the plugins directory. Blank string if no path is specified.
+	 * @param string $plugin The plugin file path to be relative to. Blank string if no plugin is specified.
+	 * @return string Filtered URL.
+	 */
+	function _cb_plugins_url_for_theme( string $url, string $path, string $plugin ): string {
+		$theme_dir = wp_normalize_path( defined( 'THEME_PATH' ) ? THEME_PATH : get_stylesheet_directory() );
+		if ( false !== strpos( $plugin, $theme_dir ) ) {
+			return \wpinc\abs_url( \wpinc\get_file_uri( dirname( $plugin ) ), $path );
+		}
+		return $url;
 	}
-	return $url;
 }
